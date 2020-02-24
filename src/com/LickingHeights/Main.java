b9b8f98edfe17c4946f52d9c8daa16f82e920847
Jaylon Slebo <jaylon.slebo.2023@student.lhschools.org>
2020-02-24 12:03:23 -0500

    Intelij commit

---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,15 @@
+package com.LickingHeights;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+        System.out.println("This is Jaylon Slebo");
+        System.out.println("over the summer I went to Myrtle beach.");
+
+
+
+
+    }
+
